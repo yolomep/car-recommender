b3e8ef91a31458f359cce078bea19da3852a10c3
yolomep <wiitong0394703@gmail.com>
2022-07-17 11:25:22 -0400

    first commit

---
+++ lib/recommender/Car.dart
@@ -0,0 +1,18 @@
+class Car {
+  int? cid;
+  String? make;
+  String? model;
+  int? year;
+  String? mpg;
+  String? transmission;
+  int? doors;
+  String? submodel;
+  int? msrp;
+  double? paidRating; //rated by paid critics
+  double? reliableRating; //rated by consumers
+  String? photo;
+
+  Car(this.cid, this.make, this.year, this.mpg, this.transmission, this.doors, this.submodel, this.msrp, this.paidRating,
+      this.reliableRating, this.photo);
+
+}
