3826ec481a3c1809336c5314f538bc3cdc696661
vodmic <strongtreestein@gmail.com>
2022-07-20 09:55:23 -0400

    made appbar. it is circular

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,52 @@
+
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'flutter demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-  // This widget is the root of your application.
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        toolbarHeight:70 ,
+        title: Text('custom App bar'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),bottomRight: Radius.circular(40), topLeft: Radius.circular(40), topRight: Radius.circular(40)) ,
+              gradient: LinearGradient(
+                  colors: [Colors.grey,Colors.orange],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter
+              )
+          ),
+        ),
+
       ),
-      home: Container(),
     );
   }
 }
