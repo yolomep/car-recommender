e107079eb7ffd01017f90b3f6de5e8316a18ee73
vodmic <strongtreestein@gmail.com>
2022-07-23 14:40:14 -0400

    finished the boxes for make,model,and price range

--- lib/main.dart
+++ lib/main.dart
@@ -144,8 +144,132 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
             ],),
             ),
-
-            DisplayText("Todays price of ${dropdownValue} gasoline"),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30.0, right: 30.0,top: 10.0,bottom: 10.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF2AD07D),
+                borderRadius: BorderRadius.circular(90),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Model",
+                        textAlign: TextAlign.center,
+                        style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        FontAwesomeIcons.commentDollar,
+                        size: 50.0,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFD9D9D9),
+                    ),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      dropdownColor:Color(0xFFDBF24B) ,
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.black,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                          pricePerGallon = gasPricesMap[dropdownValue] ?? 0.0;
+                        });
+                      },
+                      items: gasPricesMap.keys
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30.0, right: 30.0,top: 10.0,bottom: 10.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF2AD07D),
+                borderRadius: BorderRadius.circular(90),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Price Range",
+                        textAlign: TextAlign.center,
+                        style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        FontAwesomeIcons.carSide,
+                        size: 50.0,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFD9D9D9),
+                    ),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      dropdownColor:Color(0xFFDBF24B) ,
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.black,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                          pricePerGallon = gasPricesMap[dropdownValue] ?? 0.0;
+                        });
+                      },
+                      items: gasPricesMap.keys
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],),
+            ),
 
             Container(
               margin: const EdgeInsets.only(left: 50.0, right: 50.0,top: 10.0,bottom: 10.0),
@@ -231,6 +355,7 @@ class _MyHomePageState extends State<MyHomePage> {
             // ),
           ],
         ),
+
 
       ),
     );
